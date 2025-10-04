2b843c160e2704d9dddeaa0622b8f743423c2584
d0504140924-max <d0504140924@gmail.com>
2025-10-05 01:47:01 +0300

    update

--- ls_python.py
+++ ls_python.py
@@ -26,47 +26,56 @@ class Argv:
     def valid_flags(flag: str):
         return flag in Flags.__members__
 
+    @staticmethod
+    def get_double_dash_flags(args: list):
+        return list(filter(lambda flag: flag.startswith('--'), args))
 
-    def check_new_path(self, argv: list):
+    @staticmethod
+    def get_one_dash_flags(args: list):
+        double_dash_flags = []
+        double_dash = list(filter(lambda flag: flag.startswith('-'), args))
+        for flag in double_dash:
+            _flag = (l for l in flag if not l == '-')
+            double_dash_flags.append(_flag)
+        return double_dash_flags
+
+
+    def get_folder_name(self, argv: list):
         if len(argv) > 1 and not argv[-1].startswith("-"):
             if not self.valid_path(argv[-1]):
                 raise ValueError(f'invalid path {argv[-1]}')
             else:
                 return argv[-1]
-        return
+        return str(Path.cwd())
 
-    def get_folder_name(self, argv: list):
-        return str(Path.cwd()) if len(argv) == 1 else self.check_new_path(argv)
 
-    def split_argv(self, argv: list):
-        _flag = ''
+    def get_flags(self, argv: list):
         current_flags = []
-        for arg in argv:
-            if arg[2] == '-':
-                for letter in arg:
-                    if letter.isalpha():
-                        _flag += letter
-                if self.valid_flags(_flag):
-                    current_flags.append(_flag)
-                else:
-                    raise ValueError(f'invalid flag {_flag}')
-            else:
-                for letter in _flag:
+        one_dash = self.get_one_dash_flags(argv)
+        for i in one_dash:
+            for letter in i:
+                if lettre.isalpha():
                     if self.valid_flags(letter):
-                        current_flags.append(letter)
+                        current_flags.append(Flags[letter])
                     else:
-                        raise ValueError(f'invalid flag {_flag}')
+                        raise ValueError(f'invalid flag {letter}')
+        double_dash = self.get_double_dash_flags(argv)
+        for flag in double_dash:
+            if flag in Flags.__members__:
+                current_flags.append(Flags[flag])
+            else:
+                raise ValueError(f'invalid flag {flag}')
         return current_flags
 
 
     def parse_argv(self, argv: list):
-        argv1 = Args(path=self.get_folder_name(argv), flags=self.split_argv(argv))
+        argv1 = Args(path=self.get_folder_name(argv), flags=self.get_flags(argv))
         return argv1
 
 class InfoProvide:
 
     @staticmethod
-    def list_names(path: str):
+    def get_vision_files(path: str):
         visible = []
         for name in os.listdir(path):
             full_path = os.path.join(path, name)
@@ -74,16 +83,27 @@ class InfoProvide:
             if attrs != -1 and not (attrs & FILE_ATTRIBUTE_HIDDEN):
                 visible.append(name)
         return visible
-
     @staticmethod
-    def hidden_files(path: str):
-        return [n for n in os.listdir(path)]
+    def only_hidden(path: str):
+        hidden = []
+        for name in os.listdir(path):
+            full_path = os.path.join(path, name)
+            attrs = ctypes.windll.kernel32.GetFileAttributesW(str(full_path))
+            if not attrs != -1 and not (attrs & FILE_ATTRIBUTE_HIDDEN):
+                visible.append(name)
+        return hidden
 
-    def provide_names(self, args: Args):
-        if 'all' in args.flags:
-            return self.hidden_files(args.path)
+
+    def provide_files(self, args: Args):
+        if Flags.all in args.flags:
+            visibles = self.get_vision_files(args.path)
+            hidden = self.only_hidden(args.path)
+            for file in hidden:
+                visibles.append(file)
+            return visibles
+
         else:
-            return self.list_names(args.path)
+            return self.get_vision_files(args.path)
 
 class Printing:
 
