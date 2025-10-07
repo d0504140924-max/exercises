404cdadc078275a69de1083c33208ca5b4e1e4fb
d0504140924-max <d0504140924@gmail.com>
2025-10-08 01:32:01 +0300

    update

--- ls_python.py
+++ ls_python.py
@@ -26,6 +26,7 @@ class Args:
     path: str
     flags: list[Flags]
 
+
 @dataclass
 class File:
     filename: str
@@ -33,7 +34,7 @@ class File:
     time: Optional[str]
     permission: Optional[str]
 
-    def __str__(self):
+    def __str__(self)->str:
         str_to_print = f'{self.filename}'
         if self.size:
             str_to_print += f' {self.size}'
@@ -46,20 +47,8 @@ class File:
 
 @dataclass
 class Folder:
-    folder_details: File
-    details: Optional[list[Union[Folder,File]]]
-
-class AutoFlags:
-
-    @staticmethod
-    def default_flags():
-        return [Flags.color, Flags.zero]
-
-
-    def get_auto_flags(self, flags: list[Flags]):
-        default_flags = self.default_flags()
-        auto_flags = list(filter(lambda flag: not self.check_conflicting_flags(flag, flags), default_flags))
-        return set(flags + auto_flags)
+    folder_details: Optional[File]
+    files_details: Optional[list[Union['Folder',File]]]
 
 
 class CheckFlags:
@@ -67,12 +56,18 @@ class CheckFlags:
     @staticmethod
     def conflict_flags() -> dict:
         conflict_flags = {}
+        conflict_flags[Flags.permission] = []
         conflict_flags[Flags.one] =  [Flags.zero]
         conflict_flags[Flags.zero] =  [Flags.one, Flags.size, Flags.time, Flags.permission]
+        conflict_flags[Flags.all] = []
+        conflict_flags[Flags.color] = []
+        conflict_flags[Flags.directory] = []
+        conflict_flags[Flags.size] = []
+        conflict_flags[Flags.time] = []
         return conflict_flags
 
     @staticmethod
-    def to_flag(name: str):
+    def to_flag(name: str)->Flags:
         try:
             return Flags[name]
         except KeyError:
@@ -88,17 +83,32 @@ class CheckFlags:
 
     def return_conflict(self, flags: list[Flags], flag: Flags):
         conflict_flags = self.conflict_flags()
-        for key in conflict_flags:
-            if key == flag:
-                for _flag in conflict_flags[key]:
-                    if _flag in flags:
-                        raise ValueError(f'conflict{key}{_flag}')
+        for _flag in flags:
+            if _flag in conflict_flags[flag]:
+                        raise ValueError(f'conflict{flag}{_flag}')
 
-    def check_add_flag(self, current_flags: list[Flags],flag: Flags):
+    def check_add_flag(self, current_flags: list[Flags],flag: Flags)->Optional[Flags]:
         if flag in current_flags:
             return None
         self.return_conflict(current_flags, flag)
         return flag
+
+class AutoFlags:
+
+        def __init__(self, check_flags: CheckFlags):
+            self.check_flags = check_flags
+
+        @staticmethod
+        def default_flags() -> list[Flags]:
+            return [Flags.color, Flags.zero]
+
+        def get_auto_flags(self) -> list[Flags]:
+            default_flags = self.default_flags()
+            conflict_flags = self.check_flags.conflict_flags()
+            for flag in default_flags:
+                if flag in conflict_flags[flag]:
+                    default_flags.remove(flag)
+            return default_flags
 
 
 class Argv:
@@ -107,10 +117,10 @@ class Argv:
         self.check_flags = check_flags
 
     @staticmethod
-    def valid_path(path: str):
+    def valid_path(path: str)->bool:
         return Path(path).exists()
 
-    def get_double_dash_flags(self, args: list):
+    def get_double_dash_flags(self, args: list)->list[Flags]:
         valid_double_flags = []
         input_flags =  list(filter(lambda arg: arg.startswith('--'), args))
         input_flags = list(map(lambda _flag: _flag.replace('-', ''),  input_flags))
@@ -120,33 +130,31 @@ class Argv:
             valid_double_flags.append(in_flag)
         return valid_double_flags
 
-    def get_one_dash_flags(self, args: list):
+    def get_one_dash_flags(self, args: list)->list[Flags]:
         one_dash_flags = []
         input_flags = list(filter(lambda arg: arg.startswith('-') and arg[1] != '-', args))
         input_flags = list(map(lambda _flag: _flag.replace('-', ''), input_flags))
-        for flag in one_dash:
+        for flag in input_flags:
             for letter in flag:
                 in_flag =  self.check_flags.to_flag(letter)
                 self.check_flags.return_conflict(input_flags, in_flag)
                 one_dash_flags.append(in_flag)
         return one_dash_flags
 
-
-    def get_folder_name(self, path: str):
+    def get_folder_name(self, path: str)->str:
         if not self.valid_path(path):
             raise ValueError(f'invalid path {path}')
         else:
             return path
 
-    def get_flags(self, argv: list):
+    def get_flags(self, argv: list)->list[Flags]:
         current_flags = self.get_one_dash_flags(argv)
         current_flags.extend(self.get_double_dash_flags(argv))
         current_flags.extend(self.check_flags.dependant_flags(current_flags))
-        current_flags.extend(self.default_flags.get_auto_flags(current_flags))
+        current_flags.extend(self.default_flags.get_auto_flags())
         return list(set(current_flags))
 
-
-    def parse_argv(self, argv: list):
+    def parse_argv(self, argv: list)->Args:
         if len(argv) > 1 and not argv[-1].startswith("-"):
             path = self.get_folder_name(argv[-1])
         else:
@@ -160,7 +168,7 @@ FILE_ATTRIBUTE_HIDDEN = 0x2
 class InfoProvide:
 
     @staticmethod
-    def get_vision_files(path: str):
+    def get_vision_files(path: str)->list[str]:
         visible = []
         for name in os.listdir(path):
             full_path = os.path.join(path, name)
@@ -169,9 +177,8 @@ class InfoProvide:
                 visible.append(name)
         return visible
 
-
     @staticmethod
-    def only_hidden(path: str):
+    def only_hidden(path: str)->list[str]:
         hidden = []
         for name in os.listdir(path):
             full_path = os.path.join(path, name)
@@ -181,7 +188,7 @@ class InfoProvide:
         return hidden
 
     @staticmethod
-    def only_folders(path: str):
+    def only_folders(path: str)->list[str]:
         folders = []
         for name in os.listdir(path):
             full_path = os.path.join(path, name)
@@ -190,12 +197,12 @@ class InfoProvide:
         return folders
 
     @staticmethod
-    def get_size(path : str):
+    def get_size(path : str)->str:
         st = os.stat(path)
         return f"{st.st_size:}"
 
     @staticmethod
-    def get_time(path : str):
+    def get_time(path : str)->str:
         st = os.stat(path)
         dt = time.localtime(st.st_mtime)
         date_s = time.strftime("%d/%m/%Y", dt)
@@ -203,12 +210,12 @@ class InfoProvide:
         return f'[{date_s}, {time_s}]'
 
     @staticmethod
-    def get_permission(path : str):
+    def get_permission(path : str)->str:
         st = os.stat(path)
         perm = stat.filemode(st.st_mode)
         return f'{perm}'
 
-    def provide_files(self, args: Args)->list:
+    def provide_files(self, args: Args)->list[str]:
         if Flags.directory in args.flags:
             names =  self.only_folders(args.path)
             if Flags.all in args.flags:
@@ -231,20 +238,34 @@ class InfoProvide:
                 _file.time = self.get_time(full_path)
             if Flags.permission in args.flags:
                 _file.permission = self.get_permission(full_path)
-            final_names.append(_name)
-        return Folder.final_names
+            if os.path.isdir(full_path):
+                _folder = Folder(folder_details=_file, files_details=None)
+                final_names.append(_folder)
+            if not os.path.isdir(full_path):
+                final_names.append(_file)
+        folder = Folder(files_details = final_names, folder_details=None)
+        return folder
 
 
 class Printing:
 
     @staticmethod
-    def print_inline(list_names: list[File], end=' '):
+    def from_folder_to_list(folder: Folder)->list[File]:
+        list_files = []
+        for name in folder.files_details:
+            if isinstance(name, File):
+                list_files.append(name)
+            elif isinstance(name, Folder):
+                list_files.append(name.folder_details)
+        return list_files
+
+    @staticmethod
+    def print_inline(list_names: list[Union[File, str]], end=' ')->None:
         for f in list_names:
             print(f, end=end)
 
-
     @staticmethod
-    def paint_folders(list_names, base='.'):
+    def paint_folders(list_names, base='.')->list[str]:
         painted = []
         for f in list_names:
             full_path = os.path.join(base, f.filename)
@@ -254,28 +275,27 @@ class Printing:
                     painted += [f'{Fore.LIGHTWHITE_EX}{str(f)}{Style.RESET_ALL} ']
         return painted
 
-
     @staticmethod
-    def format_row(args: Args):
+    def format_row(args: Args)->str:
         if Flags.one in args.flags:
             return '\n'
         else:
             return ' '
 
-
-    def _print(self,args: Args, info: Folder):
+    def _print(self,args: Args, info: Folder)->None:
+        list_fils = self.from_folder_to_list(info)
         end_format = self.format_row(args)
         if Flags.color in args.flags:
-            painted = self.paint_folders(info.details, base=args.path)
+            painted = self.paint_folders(list_fils, base=args.path)
             return self.print_inline(painted, end=end_format)
         else:
-            return self.print_inline(info.details, end=end_format)
+            return self.print_inline(list_fils, end=end_format)
 
 
 
-def main(argv: list):
-    auto_flags = AutoFlags()
+def main(argv: list)->None:
     check_flags = CheckFlags()
+    auto_flags = AutoFlags(check_flags)
     args = Argv(auto_flags, check_flags)
     info = InfoProvide()
     printing = Printing()
